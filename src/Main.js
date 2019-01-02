bfc796dfe88faae76e41d2cec6386c2c29c521b8
Raj <rajkum@thoughtworks.com>
2019-01-02 21:34:34 +0530

    Learnt route in React.

--- src/Main.js
+++ src/Main.js
@@ -17,16 +17,16 @@ class Main extends Component {
 			<div>
 				<h1>Simple SPA</h1>
 				<ul className="header">
-					<li><NavLink to="/">Home</NavLink></li>
+					<li><NavLink exact to="/">Home</NavLink></li>
 					<li><NavLink to="/stuff">Stuff</NavLink></li>
 					<li><NavLink to="/contact">Contact</NavLink></li>
 				</ul>
 				<div className="content">
-					<Switch>
+					<div>
 						<Route exact path="/" component={Home}/>
 						<Route path="/stuff" component={Stuff}/>
 						<Route path="/contact" component={Contact}/>             
-					</Switch>
+					</div>
 				</div>
 			</div>
 		</HashRouter>
