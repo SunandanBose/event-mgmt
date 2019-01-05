4c376367974d8b57c47013b7dc8c00ec7d2e41f8
Raj <rajkum@thoughtworks.com>
2019-01-05 16:04:56 +0530

    Introducing spring security.

---
+++ backend/src/main/java/com/chargebee/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.chargebee.security;
+
+import com.chargebee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        userRepository.findByUserName(username);
+        return null;
+    }
+}
