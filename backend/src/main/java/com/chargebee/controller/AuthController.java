b91d313a234f234d24458cf2c144bee980ddfc67
Raj <rajkum@thoughtworks.com>
2019-01-06 16:55:19 +0530

    Remove unused hash

--- backend/src/main/java/com/chargebee/controller/AuthController.java
+++ backend/src/main/java/com/chargebee/controller/AuthController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
 @RestController
 public class AuthController {
     @Autowired
