05627b9ffb05ed70f2b708ff68ea0f7832d54c6e
Raj <rajkum@thoughtworks.com>
2019-02-20 20:54:34 +0530

    Base skeleton for infra

---
+++ infra/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+	version = "~> 1.59"
+	region = "us-east-1"
+}
---
+++ infra/vpc.tf
@@ -0,0 +1,48 @@
+resource "aws_vpc" "raj" {
+	tags = {
+		name = "auto-raj"
+	}
+	cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_route_table" "raj_route_table" {
+
+	vpc_id = "${aws_vpc.raj.id}"
+	route = {
+		cidr_block = "0.0.0.0/32"
+		gateway_id = "${aws_internet_gateway.gw.id}"
+	}
+
+	tags = {
+		name = "auto-raj-route-table"
+	}
+}
+
+resource "aws_internet_gateway" "gw" {
+
+	vpc_id = "${aws_vpc.raj.id}"
+
+	tags = {
+		name = "auto-raj-ig"
+	}
+}
+
+
+resource "aws_subnet" "private_subnet" {
+	vpc_id     = "${aws_vpc.raj.id}"
+	cidr_block = "10.0.1.0/24"
+
+	tags = {
+		name = "private_subnet"
+	}
+}
+
+
+resource "aws_subnet" "public_subnet" {
+	vpc_id     = "${aws_vpc.raj.id}"
+	cidr_block = "10.0.2.0/24"
+
+	tags = {
+		name = "public_subnet"
+	}
+}
