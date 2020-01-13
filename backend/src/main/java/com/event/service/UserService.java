9f4b5089730be71ceefebb6b9e25746b18040dab
Sunadan <sunandan.riders@gmail.com>
2020-01-13 21:46:21 +0530

    Hardcoding Role for signup implementation

--- backend/src/main/java/com/event/service/UserService.java
+++ backend/src/main/java/com/event/service/UserService.java
@@ -24,6 +24,7 @@ public class UserService {
     public User create(User user) {
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
+        user.setRole("ADMIN");//hardcoding role
         return userRepository.save(user);
     }
 
