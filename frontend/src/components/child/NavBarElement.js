c22c4434f0270fa9dc579be344c9a4e8a3fa1719
Sunandan Bose <sunandan.riders@gmail.com>
2020-02-16 03:14:41 +0530

    Navbar selection issue fix

--- frontend/src/components/child/NavBarElement.js
+++ frontend/src/components/child/NavBarElement.js
@@ -2,9 +2,20 @@ import React from "react";
 import { NavLink} from "react-router-dom";
 
 export default class NavBarElement extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+    handleClick(e){
+        document.getElementById('/').className=""
+        e.currentTarget.className = "active";
+        this.forceUpdate();
+    }
     render(){
         return (
-            <NavLink to={this.props.to}>{this.props.text}</NavLink>
+            <NavLink to={this.props.to} id={this.props.to} onClick={this.handleClick}>{this.props.text}</NavLink>
         );
     }
 }
